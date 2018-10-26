bd8d8f42f5f2a1084b3e2278e2b38ae43b311cd2
joneseyit <joneseyit@protonmail.com>
2018-10-26 17:04:39 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,7 @@
 def square_array(array)
   # your code here
   new_array = []
-  array.each { |num|
-    new_array << num ** 2
-
-  }
+  array.each { |num| new_array << num ** 2}
+  puts array
   new_array
 end
